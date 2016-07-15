88c423cbd71a3b5631c7e4b3cc849373c2d05785
Jon Friedman <jon@fundersclub.com>
2016-07-15 16:16:19 -0700

    revert github authentication changes... for now

--- api/views.py
+++ api/views.py
@@ -1,10 +1,6 @@
 from datetime import datetime
-import hashlib
-import hmac
 import json
-import logging
 
-from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -24,40 +20,18 @@ HANDLER_DICT = {
     'pull_request_review_comment': pull_request_review_comment_handler,
     'repository': repository_handler,
 }
-LOG = logging.getLogger(__name__)
 
 
 @csrf_exempt
 def github_callback(request):
-    # verify that this request actually comes from github
-    expected_signature = hmac.new(
-        settings.ENG_OPS_GITHUB_KEY,
-        request.body,
-        hashlib.sha1
-    ).hexdigest()
-    received_signature = request.META.get('HTTP_X_HUB_SIGNATURE', '=').split(
-        '='
-    )[1]
-    is_hmac_valid = hmac.compare_digest(expected_signature, received_signature)
-
-    if is_hmac_valid:
-        github_request = GithubRequest.objects.create(
-            body=request.body,
-            event=request.META.get('HTTP_X_GITHUB_EVENT', None),
-            method=request.method,
-            time=datetime.now(),
-        )
-
-        if 'HTTP_X_GITHUB_EVENT' in request.META:
-            handle_request(github_request)
-    else:
-        LOG.warn(
-            "unauthenticated callback: expected '{}' but got '{}'".format(
-                expected_signature,
-                received_signature,
-            )
-        )
-        LOG.warn('header is: {}'.format(request.META))
+    github_request = GithubRequest.objects.create(
+        body=request.body,
+        event=request.META.get('HTTP_X_GITHUB_EVENT', None),
+        method=request.method,
+        time=datetime.now(),
+    )
+    if 'HTTP_X_GITHUB_EVENT' in request.META:
+        handle_request(github_request)
 
     return HttpResponse()
 
