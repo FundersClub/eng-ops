04fb2408b33dc69edd799501e556b522cb7d671d
thomasrockhu <tom@fundersclub.com>
2016-07-21 10:42:30 -0700

    Let login be null

--- user_management/models.py
+++ user_management/models.py
@@ -1,10 +1,12 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
 class GithubUser(models.Model):
 
     id = models.PositiveIntegerField(primary_key=True)
-    login = models.CharField(max_length=100)
+    # login = ArrayField(models.CharField(max_length=100, null=True))
+    login = models.CharField(max_length=100, null=True)
     slack_username = models.CharField(max_length=100, null=True)
 
     def __unicode__(self):
