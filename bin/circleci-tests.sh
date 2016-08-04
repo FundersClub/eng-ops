9a872012c16d7a907dd98b57ef58826131cadf81
Kenny Bambridge <kenny@fundersclub.com>
2016-08-04 12:04:43 -0700

    Revert "Add circle.yml and circleci test script"
    This reverts commit b745916f8fde9b306f7220c13c4ed96f00dacb58.

--- bin/circleci-tests.sh
+++
@@ -1,6 +0,0 @@
-set -e
-run --concurrency multiprocessing --parallel-mode manage.py test --skip-migrations --noinput --split-to-bins=$CIRCLE_NODE_TOTAL --test-bin=$CIRCLE_NODE_INDEX --parallel 2
-# TODO: Set up coverage tracking
-# coverage combine
-# coverage report
-# bash <(curl -s https://codecov.io/bash) -t <token>
