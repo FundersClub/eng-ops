b6db7b2c23c185256d397e948226d8196a03f93d
Charley Walton <chazeah@gmail.com>
2017-03-23 18:06:27 -0700

    breaking out into table, making configurable

--- repositories/admin.py
+++ repositories/admin.py
@@ -1,6 +1,20 @@
+from datetime import (
+    datetime,
+    timedelta,
+)
+
+from django.conf.urls import (
+    patterns,
+    url,
+)
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+from django.db.models import Case, When
 from django.http.request import HttpRequest
-from django.template.loader import render_to_string
+from django.shortcuts import render
+from django.utils import timezone
+from django.utils.html import format_html
+from django.views.decorators.csrf import csrf_exempt
 
 from labels.models import Label
 from issues.models import Issue
@@ -16,17 +30,18 @@ class RepositoryAdmin(admin.ModelAdmin):
         'id',
         'name',
         'private',
-        'labels_report'
+        'labels_report_link',
     ]
     list_display = [
         'name',
         'public',
+        'labels_report_link',
     ]
     readonly_fields = [
         'id',
         'name',
         'private',
-        'labels_report',
+        'labels_report_link',
     ]
 
     def has_add_permission(self, request, obj=None):
@@ -39,9 +54,32 @@ class RepositoryAdmin(admin.ModelAdmin):
     def public(self, obj):
         return not obj.private
 
-    def labels_report(self, obj):
+    def labels_report_link(self, obj):
+        return format_html(
+            u'<a href={}>{}</a>'.format(
+                reverse('admin:labels-report', args=(obj.id, )),
+                'View Labels Report',
+            )
+        )
+
+    def get_urls(self):
+        urls = super(RepositoryAdmin, self).get_urls()
+        my_urls = [
+            url(r'^(.+)/labels-report/$', self.admin_site.admin_view(self.labels_report), name='labels-report'),
+        ]
+        return my_urls + urls
+
+    @csrf_exempt
+    def labels_report(self, request, repository_id):
+        today = timezone.now().date()
+        start_date_str = request.GET.get('start_date', None)
+        start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date() if start_date_str else today - timedelta(days=14)
+        end_date_str = request.GET.get('end_date', None)
+        end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date() if end_date_str else today
+
         # We only care about a specific subset of all labels.  Listing
         # labels by ids to not have to worry about unicode stuff.
+        repo = Repository.objects.get(id=repository_id)
         label_ids = [
             43,  # 30 effort task
             34,  # 90 effort task
@@ -58,15 +96,24 @@ class RepositoryAdmin(admin.ModelAdmin):
             48,  # feature
             44,  # support
         ]
-        labels = Label.objects.filter(id__in=label_ids).order_by('name')
+        user_names = [
+            'moofive',
+            'chazeah',
+            'MrJaeger',
+            'eranrund',
+            'thomasrockhu',
+        ]
+
+        preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(label_ids)])
+        labels = Label.objects.filter(id__in=label_ids).order_by(preserved)
         issues = (Issue.objects
-            .filter(closed_at__isnull=False, labels__in=labels, repository=obj)
+            .filter(closed_at__isnull=False, labels__in=labels, repository=repo)
+            .filter(closed_at__gte=start_date, closed_at__lte=end_date)
+            .filter(assignee__logins__overlap=user_names)
             .distinct()
         )
-
         users = (GithubUser.objects
             .filter(id__in=issues.values_list('assignee', flat=True))
-            .order_by('slack_username')
             .distinct()
         )
         user_data = []
@@ -76,14 +123,16 @@ class RepositoryAdmin(admin.ModelAdmin):
             for label in labels:
                 user_row.append(issues.filter(assignee=user, labels=label).count())
 
-            user_data.append([user.slack_username] + user_row)
+            user_data.append([user.__unicode__()] + user_row)
 
         totals = []
         for label in labels:
             totals.append(issues.filter(assignee__in=users, labels=label).count())
 
-        return render_to_string('labels_report_table.html', context={
+        return render(request, 'labels_report_table.html', {
             'labels': labels,
             'totals': totals,
             'user_data': user_data,
+            'start_date': start_date.strftime('%Y-%m-%d'),
+            'end_date': end_date.strftime('%Y-%m-%d'),
         })
